9bddf7ef5219e858c5263081fde7805d84dba243
Ben Grabow <bgrabow@splashfinancial.com>
2021-12-08 16:07:02 -0600

    Implement gradient descent. Implement median. Use gradient descent and `mean` initial guess for part-2.

--- src/aoc/day_07.clj
+++ src/aoc/day_07.clj
@@ -27,6 +27,29 @@
        (sort-by second)
        (ffirst)))
 
+(defn descend-kvs
+  [kvs]
+  (->> (partition 2 1 kvs)
+       (drop-while #(>= (second (first %)) (second (second %))))
+       (ffirst)))
+
+(defn gradient-descent
+  "Given a fitness function, f, and a starting position, x0 an integer, find
+  the tuple, [x (f x)], that minimizes (f x) within the optimization valley
+  containing x0."
+  [f x0]
+  (assert (zero? (rem x0 1)))
+  (let [dx (range)
+        leftward (map #(- x0 %) dx)
+        rightward (map #(+ x0 %) dx)
+        least-left (->> (map f leftward)
+                        (map vector leftward)
+                        (descend-kvs))
+        least-right (->> (map f rightward)
+                         (map vector rightward)
+                         (descend-kvs))]
+    (->> (sort-by second [least-left least-right])
+         (first))))
 
 (defn part-1
   []
@@ -39,14 +62,26 @@
                            (range (apply min crabs)
                                   (inc (apply max crabs)))))))
 
-(def triangular-fuel-cost
-  (memoize
-    (fn [dx]
-      (let [dx (Math/abs ^long dx)]
-        (case dx
-          0 0
-          1 1
-          (+ dx (triangular-fuel-cost (dec dx))))))))
+(defn median
+  [xs]
+  (let [n (count xs)]
+    (if (even? n)
+      (mean (take 2 (drop (dec (quot n 2)) (sort xs))))
+      (nth (sort xs) (quot n 2)))))
+
+(comment
+  (time (let [crabs (parse-input input)]
+          (gradient-descent
+            (partial fuel-required crabs)
+            (median crabs))))
+  (time (let [crabs (parse-input input)]
+          (gradient-descent
+            (partial fuel-required crabs)
+            0))))
+
+(defn triangular-fuel-cost
+  [dx]
+  (/ (* dx (inc dx)) 2))
 
 (defn triangular-fuel-required
   [crabs destination]
@@ -56,12 +91,21 @@
 (defn part-2
   []
   (time (let [crabs (parse-input input)]
-          (triangular-fuel-required
-            crabs
-            (exhaustive-search
-              (partial triangular-fuel-required crabs)
-              (range (apply min crabs)
-                     (inc (apply max crabs))))))))
+          (second (gradient-descent
+                    (partial triangular-fuel-required crabs)
+                    (int (mean crabs)))))))
+
+(comment
+  (time (let [crabs (parse-input input)]
+          (gradient-descent
+            (partial triangular-fuel-required crabs)
+            0)))
+  (time (let [crabs (parse-input input)]
+          (triangular-fuel-required crabs 0)))
+  (time (let [crabs (parse-input input)]
+          (gradient-descent
+            (partial triangular-fuel-required crabs)
+            (int (mean crabs))))))
 
 (comment
   (let [crabs (parse-input example-input)]
