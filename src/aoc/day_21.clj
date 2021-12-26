9fa1d04dc75c72838dc0f50a75215713751b38c0
Ben Grabow <ben.grabow@gmail.com>
2021-12-26 10:51:30 -0600

    Solve d21p2

--- src/aoc/day_21.clj
+++ src/aoc/day_21.clj
@@ -1,5 +1,6 @@
 (ns aoc.day-21
-  (:require [aoc.util :as util]))
+  (:require [aoc.util :as util]
+            [clojure.math.combinatorics :as combinatorics]))
 
 (def input (util/read-input))
 
@@ -17,6 +18,12 @@
 (defn update-player
   [player rolls]
   (let [position (to-space (+ (:position player) (reduce + rolls)))]
+    {:position position
+     :score (+ (:score player) position)}))
+
+(defn update-player'
+  [player step-size]
+  (let [position (to-space (+ (:position player) step-size))]
     {:position position
      :score (+ (:score player) position)}))
 
@@ -49,5 +56,36 @@
        (lower-score (last turns)))))
 
 (comment
+  (* 10 10 22 22 2))
+
+(comment
   (def wrong-answers
     {:too-low #{171500}}))
+
+(def dirac-turn-frequencies
+  (frequencies (map #(apply + %) (combinatorics/permuted-combinations [1 1 1 2 2 2 3 3 3] 3))))
+
+(defn dirac-step
+  [state step-size]
+  (-> state
+      (update (:current-player state) update-player' step-size)
+      (update :current-player next-player)))
+
+(def win-counts
+  (memoize
+    (fn [state]
+      (cond
+        (<= 21 (get-in state [:player-1 :score])) {:player-1 1 :player-2 0}
+        (<= 21 (get-in state [:player-2 :score])) {:player-1 0 :player-2 1}
+        :else (->> (for [[n-spaces count] dirac-turn-frequencies]
+                     (util/map-vals
+                       (win-counts (dirac-step state n-spaces))
+                       #(* count %)))
+                   (apply merge-with +))))))
+
+(defn part-2
+  []
+  (time (apply max ((juxt :player-1 :player-2)
+                    (win-counts {:player-1       {:score 0 :position 8}
+                                 :player-2       {:score 0 :position 9}
+                                 :current-player :player-1})))))
