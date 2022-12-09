64783eec7293c71525b138a60e4581d292ff1b14
Ben Grabow <bgrabow@splashfinancial.com>
2022-12-09 12:31:50 -0600

    2022.07 cleanup

--- src/aoc/2022/day_07.clj
+++ src/aoc/2022/day_07.clj
@@ -1,6 +1,5 @@
 (ns aoc.2022.day-07
-  (:require [clojure.string :as str]
-            [clojure.walk :as walk]))
+  (:require [clojure.string :as str]))
 
 (def input (slurp "resources/2022/input07.txt"))
 
@@ -24,26 +23,19 @@
 (defn cd
   [state dest]
   (case dest
-    ".." (-> state
-             #_(assoc-in [:files (peek (:path state))]
-                 (->> (get-in state [:dirs (peek (:path state))])
-                      (map #(get-in state [:files %]))
-                      (reduce +)))
-             (update :path pop))
+    ".." (update state :path pop)
     (update state :path (fnil conj []) dest)))
 
 (defn file
   [state filename size]
   (-> state
       (update :files assoc (conj (:path state) filename) size)
-      (update-in [:dirs (:path state)] (fnil conj #{}) filename)
-      #_(assoc-in (concat [:tree] (reverse (:path state)) [filename]) {:filename filename :size size})))
+      (update-in [:dirs (:path state)] (fnil conj #{}) filename)))
 
 (defn dir
   [state dirname]
   (-> state
-      (update-in [:dirs (:path state)] (fnil conj #{}) dirname)
-      #_(assoc-in (concat [:tree] (reverse (:path state)) [:dirname]) dirname)))
+      (update-in [:dirs (:path state)] (fnil conj #{}) dirname)))
 
 (defn operate
   [state {:keys [op filename dirname dest size]}]
@@ -53,51 +45,12 @@
     :ls state
     :file (file state filename size)))
 
-(defn return-to-root
-  [state]
-  (let [depth (count (:path state))]
-    (reduce operate state (repeat depth {:op :cd :dest ".."}))))
-
-(defn dir?
-  [state filename]
-  (boolean (get-in state [:dirs filename])))
-
-(defn file?
-  [state filename]
-  (not (boolean (get-in state [:dirs filename]))))
-
 (defn dirsize
   [state dirpath]
   (->> (for [filename (get-in state [:dirs dirpath])]
          (or (get-in state [:files (conj dirpath filename)])
              (dirsize state (conj dirpath filename))))
        (reduce +)))
-
-#_(defn solve-1
-    [s]
-    (let [state (return-to-root (reduce operate {} (parse-instructions s)))]
-      #_(->> (keys (:dirs state))
-             (random-sample 0.5)
-             (map (partial dirsize state))
-             (filter #(<= % 100000))
-             (reduce +))
-      (->> (walk/postwalk
-             (fn [node]
-               (if (and (map? node)
-                     (not (:size node)))
-                 (assoc node :size (reduce + (keep :size (vals node))))
-                 node))
-             (:tree state))
-           (tree-seq
-             :size
-             #(filter :dirname (vals %)))
-           (filter #(<= (:size %) 100000))
-           (map :size)
-           (reduce +))
-      (->> (for [[filename size] (:files state)
-                 :when (and (<= size 100000)
-                            (dir? state filename))]
-             [filename size]))))
 
 (defn solve-1
   [s]
