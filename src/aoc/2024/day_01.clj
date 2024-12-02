830c1094d8c30b41df27a1cf92a613545637c478
Ben Grabow <bgrabow@splashfinancial.com>
2024-12-02 12:53:39 -0600

    Extract common fn

--- src/aoc/2024/day_01.clj
+++ src/aoc/2024/day_01.clj
@@ -1,22 +1,22 @@
 (ns aoc.2024.day-01
   (:require [aoc.util :as util]))
 
+(defn parse-lists
+  []
+  (->> (re-seq #"\d+" (util/read-input))
+    (map parse-long)
+    (partition 2)
+    (util/transpose)
+    (map sort)))
+
 (defn part-1
   []
-  (let [[left right] (->> (re-seq #"\d+" (util/read-input))
-                       (map parse-long)
-                       (partition 2)
-                       (util/transpose)
-                       (map sort))]
+  (let [[left right] (parse-lists)]
     (reduce + (map (comp abs -) left right))))
 
 (defn part-2
   []
-  (let [[left right] (->> (re-seq #"\d+" (util/read-input))
-                       (map parse-long)
-                       (partition 2)
-                       (util/transpose)
-                       (map sort))]
+  (let [[left right] (parse-lists)]
     (->> (select-keys (frequencies right) left)
       (map (fn score [[val freq]] (* val freq)))
       (reduce +))))
